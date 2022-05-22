bf971b1b2b1f0fa37c2267b71b7a5f1627a6f135
unknown <adityagautam18@gmail.com>
2022-05-23 01:17:59 +0530

    Added my project

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,16 @@
 import merge from 'lodash/merge';
 import registry from '@balancer-labs/assets/generated/pm/registry.homestead.json';
 import registryKovan from '@balancer-labs/assets/generated/pm/registry.kovan.json';
+import registryWarringstakes from '@balancer-labs/assets/generated/pm/registry.Warringstakes.json';
 import homestead from '@/config/homestead.json';
 import kovan from '@/config/kovan.json';
+import Warringstakes from '@/config/Warringstakes.json';
 
-const configs = { homestead, kovan };
+const configs = { homestead, kovan, Warringstakes };
 configs.homestead = merge(registry, configs.homestead);
 configs.kovan = merge(registryKovan, configs.kovan);
-const network = process.env.VUE_APP_NETWORK || 'kovan';
+configs.Warringstakes = merge(registryWarringstakes, configs.Warringstakes);
+const network = process.env.VUE_APP_NETWORK || 'Warringstakes';
 const config = configs[network];
 config.env = process.env.VUE_APP_ENV || 'staging';
 
